035d3fe928f94034d3ab186ac6bd0761a935c26c
Appar <32193913+appars@users.noreply.github.com>
2019-02-14 15:08:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def main():
 
 @app.route('/how are you')
 def hello():
-    return 'I am good, Thank you. How about you?'
+    return 'I am good, Thanks. How about you?'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
